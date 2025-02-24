2746a77edb5be762f107f8b431db3adad1d0eac8
Wylliam <chengwaiyuen27112003@gmail.com>
2025-02-24 18:46:14 -0500

    Restructure the server directory - FINAL FILE STRUCTURE

---
+++ server/router.py
@@ -0,0 +1,18 @@
+# router.py
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from server.config import get_firebase_user_from_token
+
+router = APIRouter()
+
+
+@router.get("/")
+def hello():
+    """Hello world route to make sure the app is working correctly"""
+    return {"msg": "Hello World!"}
+
+
+@router.get("/userid")
+async def get_userid(user: Annotated[dict, Depends(get_firebase_user_from_token)]):
+    """gets the firebase connected user"""
+    return {"id": user["uid"]}
