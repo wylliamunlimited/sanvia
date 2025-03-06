0eb0ac343837c5e895ebee5e8815df224acbe31a
Wylliam <chengwaiyuen27112003@gmail.com>
2025-03-06 16:41:39 -0500

    Deployment Bug Fix: Removing unnecessary imports

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css'
