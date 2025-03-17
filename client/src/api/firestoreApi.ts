9da3d0c291dc2374e0c94030e2963814d23c2bc0
Wylliam <chengwaiyuen27112003@gmail.com>
2025-03-17 18:51:06 -0400

    Enabling user to make edits on profile information

--- client/src/api/firestoreApi.ts
+++ client/src/api/firestoreApi.ts
@@ -39,7 +39,7 @@ api.interceptors.request.use((config) => {
 });
 
 export const firestoreApi = {
-    uploadSurvey: async (age: string, gender: string, sex: string, height: string, weight: string): Promise<any> => {
+    uploadProfile: async (age: string, gender: string, sex: string, height: string, weight: string): Promise<any> => {
         try {
             const response = await api.post<any>('/survey', { 
                 "age": age,
