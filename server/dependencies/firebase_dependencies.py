15734127eae2a7dc65838ba74a1b6521160d34d1
Wylliam <chengwaiyuen27112003@gmail.com>
2025-03-17 12:32:25 -0400

    Survey data upload implemented

--- server/dependencies/firebase_dependencies.py
+++ server/dependencies/firebase_dependencies.py
@@ -39,8 +39,13 @@ def get_firestore_client():
 ## this function should be under utils for sign up or database operation script [will come back and check]
 def update_survey_entry(user_id: str, survey_data: Survey):
     """Updates a user's survey entry in Firestore."""
-    doc_ref = get_firestore_client().collection("surveys").document(user_id)
+    doc_ref = get_firestore_client().collection("profiles").document(user_id)
     doc_ref.set(survey_data.to_dict(), merge=True)
+
+def update_name_entry(user_id: str, first_name: str, last_name: str):
+    """Updates a user's first & last names in Firestore."""
+    doc_ref = get_firestore_client().collection("profiles").document(user_id)
+    doc_ref.set({"first-name": first_name, "last-name": last_name}, merge=True)
 
 def update_chat_entry(user_id: str, thread_id: str, chat_data: dict):
     """Update chat data of thread_id in Firestore"""
@@ -85,7 +90,9 @@ def get_firebase_user_from_token(
                 detail="Token Required.",
                 headers={"WWW-Authenticate": "Bearer realm='Authentication Required'"},
             )
-        user = verify_id_token(token.credentials)
+        user = verify_id_token(token.credentials, check_revoked=True)
+        
+        # print("Decoded Firebase User:", user)
         return user
     except Exception:
         raise HTTPException(
