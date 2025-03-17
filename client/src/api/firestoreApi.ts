7b2ff0be52a7b0692aa8096c70b1418fc322be3b
Wylliam <chengwaiyuen27112003@gmail.com>
2025-03-17 14:05:31 -0400

    Commenting out unused variables on Frontend for deployment build

--- client/src/api/firestoreApi.ts
+++ client/src/api/firestoreApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
-const TEST_AUTH_TOKEN = import.meta.env.VITE_TEST_AUTH_TOKEN;  // Testing
+// const TEST_AUTH_TOKEN = import.meta.env.VITE_TEST_AUTH_TOKEN;  // Testing
 
 export enum Gender {
     Men = "Men",
