b9eec147f8d022555ec52491e5e741c75117649d
Oghenerukevwe Omusi <ojomusi@bu.edu>
2025-03-20 23:07:04 -0400

    add tavily search categories, complex and simple response, and see more

--- server/dependencies/tavily_dependencies.py
+++ server/dependencies/tavily_dependencies.py
@@ -6,8 +6,29 @@ sys.path.insert(2, "../constants")
 from tavily import TavilyClient
 from constants.credentials import TAVILY_API_KEY
 
-# Initialize Tavily client
-def get_tavily_client():
-    """Return Tavily client."""
-    return TavilyClient(api_key=TAVILY_API_KEY)
+# List of trusted sources
+TRUSTED_SOURCES = [
+    "pubmed.ncbi.nlm.nih.gov",
+    "jamanetwork.com",
+    "nejm.org",
+    "wolterskluwer.com",
+    "dynamed.com",
+    "ama-assn.org",
+    "cdc.gov",
+    "who.int",
+    "medscape.com",
+    "clinicaltrials.gov",
+    "fda.gov",
+    "ema.europa.eu",
+    "nice.org.uk",
+    "embase.com",
+    "cochranelibrary.com",
+    "ncbi.nlm.nih.gov/pmc",
+    "europepmc.org",
+    "uptodate.com",
+]
 
+
+# Initialize Tavily client with domain restrictions
+def get_tavily_client():
+    return TavilyClient(api_key=TAVILY_API_KEY, allowed_domains=TRUSTED_SOURCES)
