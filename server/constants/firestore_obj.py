b9049444ed68ef7ee24ac6ab869f4e7ced7b31bf
Wylliam <chengwaiyuen27112003@gmail.com>
2025-04-06 18:30:36 -0400

    Implemented and tested RAG logics, document intelligent query ready to go

--- server/constants/firestore_obj.py
+++ server/constants/firestore_obj.py
@@ -54,3 +54,11 @@ class Survey:
     def __repr__(self):
         return (f'Survey(age={self.age}, gender={self.gender}, sex={self.sex}, height={self.height}, weight={self.weight},' 
                 f'conditions={self.conditions}, medications={self.medications}, onboarding={self.onboarding})')
+    
+    def contextualize(self):
+        f"""Patient Profile:
+                Age: {self.age}
+                Gender: {self.gender}
+                Sex: {self.sex}
+                Height: {self.height} cm
+                Weight: {self.weight} kg"""
