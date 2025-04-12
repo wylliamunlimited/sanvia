76eeb5fbfe3725667c50eb3c78caab28448d04de
Wylliam <chengwaiyuen27112003@gmail.com>
2025-04-12 01:43:34 -0400

    Fixing new survey field (Conditions, Medications) input intake

--- client/src/api/firestoreApi.ts
+++ client/src/api/firestoreApi.ts
@@ -39,7 +39,7 @@ api.interceptors.request.use((config) => {
 });
 
 export const firestoreApi = {
-    uploadProfile: async (age: string, gender: string, sex: string, height: string, weight: string, conditions: string[], medications: []): Promise<any> => {
+    uploadProfile: async (age: string, gender: string, sex: string, height: string, weight: string, conditions: string[], medications: string[]): Promise<any> => {
         try {
             const response = await api.post<any>('/survey', { 
                 "age": age,
