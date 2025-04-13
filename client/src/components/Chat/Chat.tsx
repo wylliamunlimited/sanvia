a954cbedfc4aac779cf68291bca2a745d02315d5
Yaseminnurlu <yaseminn@bu.edu>
2025-04-13 16:39:11 -0400

    making changes based on review

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -236,7 +236,7 @@ const Chat = () => {
               <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20" />
             </svg>
           </button>
-          <button type="submit" disabled={isThinking}>
+          <button className="submit-button" type="submit" disabled={isThinking}>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 2L12 20M12 2L5 9M12 2L19 9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
             </svg>
