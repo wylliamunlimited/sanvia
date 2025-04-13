5b5af6df20eac8315ed76a9f8a6f0209bca90ce9
Yaseminnurlu <yaseminn@bu.edu>
2025-04-13 11:46:04 -0400

    deleted settings

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -175,7 +175,7 @@ const Chat = () => {
       <div className="chat-header">
         <Header 
           icon={
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <svg className="chat-logo" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
             </svg>
           }
--- client/src/components/History/History.tsx
+++ client/src/components/History/History.tsx
@@ -123,7 +123,7 @@ const History = () => {
 
   return (
     <div className="history-content">
-      <div className= "chat-header">
+      <div className= "history-header">
       <Header 
         icon={
           <svg className = "history-logo" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
