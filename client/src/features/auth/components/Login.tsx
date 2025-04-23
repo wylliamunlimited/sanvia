a38aef3042c14ec44ffef540f0f4b825a39f1d02
Wylliam <chengwaiyuen27112003@gmail.com>
2025-04-23 12:43:39 -0400

    Message encryption is completed

--- client/src/features/auth/components/Login.tsx
+++ client/src/features/auth/components/Login.tsx
@@ -4,6 +4,7 @@ import "./auth.css";
 import { useFirebase } from "../../../context/FirebaseContext";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { handleFormNavigation } from "../../../shared/utils/formNavigation";
+import securityApi from "../../../api/encryption/security";
 
 interface LoginProps {
   onLoginSuccess: () => void;
@@ -32,6 +33,11 @@ const Login: React.FC<LoginProps> = ({ onLoginSuccess }) => {
           user.getIdToken(true).then((token) => {
             localStorage.setItem("sanvia-refreshToken", token);
             console.log("Token is stored properly.");
+          });
+
+          securityApi.getEncryptionKey().then((key) => {
+            sessionStorage.setItem("AES_KEY", key);
+            console.log("AES Encryption Key stored properly.");
           });
           
           onLoginSuccess();
