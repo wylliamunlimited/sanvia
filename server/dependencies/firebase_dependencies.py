d529cbf7e6a411c0df3e0c73b5d518bf0e94d329
Wylliam <chengwaiyuen27112003@gmail.com>
2025-04-28 14:08:20 -0400

    Finalizing changes for design implementation

--- server/dependencies/firebase_dependencies.py
+++ server/dependencies/firebase_dependencies.py
@@ -50,6 +50,7 @@ def update_profile(user_id: str, survey_data: Survey) -> None:
     """Updates a user's profile in Firestore."""
     try:
         doc_ref = get_firestore_client().collection("profiles").document(user_id)
+        ## Cleaning data in case of insufficient data
         cleaned_data = {k: v for k, v in survey_data.to_dict().items() if v is not None}
         doc_ref.set(cleaned_data, merge=True)
     except Exception as e:
