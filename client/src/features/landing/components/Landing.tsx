aab67c515ad03ac85b2c3c7e5bcaf81174d30036
Yaseminnurlu <yaseminn@bu.edu>
2025-04-30 20:26:50 -0400

    styling the landing page

--- client/src/features/landing/components/Landing.tsx
+++ client/src/features/landing/components/Landing.tsx
@@ -30,10 +30,10 @@ const Landing: React.FC = () => {
 
       <main className="hero-section">
         <h1 className="hero-title">
-          <span className="hero-text-italic">Your&nbsp;</span>
+          <span className="hero-text-bold">Secure&nbsp;</span>
           <span className="hero-text-regular"> Health,</span>
           <br />
-          <span className="hero-text-bold">Clearly</span>
+          <span className="hero-text-bold">Secure</span>
           <span className="hero-text-regular"> Explained</span>
         </h1>
         
@@ -47,7 +47,7 @@ const Landing: React.FC = () => {
           className="cta-button"
           onClick={() => navigate('/auth/signup')}
         >
-          Ask Sanvia
+          Chat with Sanvia
         </button>
       </main>
     </div>
